459a8bc1410470681e3a075c3ccc260d5cdc4e9e
Jayvee Ramos <97137481+TechJayVee@users.noreply.github.com>
2023-10-25 11:03:36 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { AppDataSource } from "./data-source";
+import userRouter from "./router/userRouter";
+
+AppDataSource.initialize()
+  .then(async () => {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use("/api", userRouter);
+    app.listen(3000);
+
+    console.log("Express server has started on port 3000.");
+  })
+  .catch((error) => console.log(error));
